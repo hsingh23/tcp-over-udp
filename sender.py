eb4ff7f5c26405e6a3e5ded1f89ae3444bf70706
Harsh Singh <hsingh23@illinois.edu>
2013-11-16 22:27:16 -0600

    sender prints num_acks

--- sender.py
+++ sender.py
@@ -11,6 +11,7 @@ class TCPStateMachine(object):
     def __init__(s, file_name, udp, destination):
         super(TCPStateMachine, s).__init__()
         s.current_state = State.slow_start
+        s.ack_count = 0
         chunks = s.chunkify_file(file_name, 100)
         s.window = Window(**{"MSS": 100.0, "ssthresh": 1000.0, "max_sequence_number": 9000, "max_cwnd": 25, "timeout_length": 1.0, "state_machine": s, "chunks": chunks, "udp": udp, "destination": destination})
         s.window.transmit_as_allowed()
@@ -35,12 +36,13 @@ class TCPStateMachine(object):
 
     def run(s, event):
         if event.name == "ack":
-            print "Got %s" % event.data
             ack_num = s.parse_segment(event.data)
             event = s.window.add_ack(ack_num)
+            if event:
+                s.ack_count += 1
         if event:
-            if event.name == "timeout":
-                print s.window.timeout_length
+            # if event.name == "timeout":
+            #     print s.window.timeout_length
             s.current_state = s.current_state.next(event, s.window)
 
 
@@ -61,6 +63,7 @@ def main(argv):
         f.writelines(t.window.trace_file)
     with open("cwnd-%s" % file_name, "w") as f:
         f.writelines(t.window.cwnd_file)
+    print t.ack_count
 
 
 if __name__ == "__main__":
